016cd95de3490e593cf07c4fbfb44373e8a5f413
mattiasakesson <33224977+mattiasakesson@users.noreply.github.com>
2023-10-11 16:33:21 +0200

    Update init_venv.sh

--- bin/init_venv.sh
+++ bin/init_venv.sh
@@ -7,7 +7,7 @@ python -m venv .assist-pytorch-venv
 # Pip deps
 .assist-pytorch-venv/bin/pip install --upgrade pip
 .assist-pytorch-venv/bin/pip install -r requirements.txt
-#git clone https://github.com/scaleoutsystems/fedn.git
+git clone https://github.com/scaleoutsystems/fedn.git
 cd fedn
 git checkout release/v0.5.0
 cd ..
