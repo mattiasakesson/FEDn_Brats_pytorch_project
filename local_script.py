3806585ede700e0954d2b005b0d957ba2c7f2d38
mattiasakesson <ma10s@hotmail.com>
2024-03-13 11:26:55 +0100

    add local script

---
+++ local_script.py
@@ -0,0 +1,45 @@
+from client.entrypoint import train, validate
+import os
+import fire
+
+
+def validate_local():
+
+    model_folder = '/var/modelfolder'
+    result_folder = '/var/resultfolder'
+
+    model_versions = [f for f in os.listdir(model_folder) if f.endswith('.npz')]
+    print("model versions: ", model_versions)
+    for model_name in model_versions:
+        print()
+        print()
+        epoch = model_name[:-4]
+
+
+
+        model_path = os.path.join(model_folder,model_name)
+        result_path = os.path.join(result_folder,epoch)
+        print("model_path: ", model_path)
+        print("epoch: ", epoch)
+        validate(model_path, result_path, data_path='/var/data', client_settings_path='/var/client_settings.yaml')
+
+
+def train_local():
+
+    model_folder = '/var/modelfolder'
+
+    in_model_path = '/var/seed.npz'
+    for epoch in range(100):
+        print()
+        print()
+        print("epoch: ", epoch)
+        print("list model_folder: ", os.listdir(model_folder))
+        out_model_path = os.path.join(model_folder,str(epoch) + '.npz')
+        train(in_model_path, out_model_path, data_path='/var/data', client_settings_path='/var/client_settings.yaml')
+        in_model_path = out_model_path
+
+if __name__ == '__main__':
+    fire.Fire({
+        'train': train_local,
+        'validate': validate_local,
+    })
